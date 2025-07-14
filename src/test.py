34e2a32b5b0c600cc3bc3977f1ddcd70a73edd96
MaximSI81 <safonovmio@gmail.com>
2025-07-14 23:58:58 +0000

    изменения test

--- src/test.py
+++ src/test.py
@@ -2,18 +2,26 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, udf
 from pyspark.sql.types import StructType, StructField, StringType, LongType
 import json
+import os
 
 spark = SparkSession.builder \
     .appName("KafkaBinaryDecoder") \
     .config("spark.ui.port", "4045") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,"
+           "org.apache.hadoop:hadoop-aws:3.3.4"
+                                    ) \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9006") \
+    .config("spark.network.timeout", "600s") \
+    .config("spark.hadoop.fs.s3a.access.key", os.getenv("MINIO_ROOT_USER")) \
+    .config("spark.hadoop.fs.s3a.secret.key", os.getenv("MINIO_ROOT_PASSWORD")) \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .getOrCreate()
 
 # 1. Чтение данных из Kafka
 df = spark \
     .read \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("kafka.bootstrap.servers", "10.130.0.25:9092") \
     .option("subscribe", "cdc.stg_analytical.action_users") \
     .option("startingOffsets", "earliest") \
     .load() \
@@ -51,8 +59,11 @@ df.select(from_json(col("value"), schema).alias("t")) \
         "t.payload.after.name_link",
         "t.payload.after.action_type",
         "t.payload.after.purchase_amount") \
-  .show(truncate=False) 
-
+    .write \
+    .format("parquet") \
+    .mode("append") \
+    .option("path", f"s3a://{os.getenv("MINIO_PROD_BUCKET_NAME")}/data5/") \
+    .save()
 
 
 spark.stop()
